599560e7d014c58792fb204968ff53dd8d53848b
Lan <lanciiu>
2020-07-01 16:47:45 +0700

    add code mau

---
+++ app/src/main/java/com/example/hoclaptrinh/ItemCodeMau.java
@@ -0,0 +1,14 @@
+package com.example.hoclaptrinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemCodeMau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_code_mau);
+    }
+}
